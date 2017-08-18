aa03fa3bedd37845471b410ed18f72dcc138788a
Pedro Perez <pperezm@itesm.mx>
2017-08-18 09:55:26 -0500

    week2-session2

---
+++ basic-io/example1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+	int i;
+	
+	printf("params = %i\n", argc);
+	for (i = 0; i < argc; i++) {
+		printf("args[%i] = %s\n", i, argv[i]);
+	}
+	return 0;
+}
