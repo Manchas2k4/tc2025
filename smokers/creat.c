0205574df71982b19ced58907c3ddc45bc765a85
Pedro Perez <pperezm@itesm.mx>
2018-10-23 09:04:30 -0500

    smokers

---
+++ smokers/creat.c
@@ -0,0 +1,35 @@
+#include "header.h"
+
+int main(int argc, char* argv[]) {
+	key_t key;
+	int semid, i;
+	unsigned short values[4];
+
+	if (argc != 1) {
+		printf("usage: %s\n", argv[0]);
+		return -1;
+	}
+
+	if ( (key = ftok("/dev/null", 200)) == (key_t) -1 ) {
+		perror(argv[0]);
+		return -1;
+	}
+
+	if ( (semid = semget(key, 4, 0666 | IPC_CREAT)) < 0 ) {
+		perror(argv[0]);
+		return -1;
+	}
+
+	semctl(semid, TABLE, SETVAL, 1);
+	semctl(semid, TWENTY, SETVAL, 0);
+	semctl(semid, FIFTY, SETVAL, 0);
+	semctl(semid, HUNDRED, SETVAL, 0);
+
+	semctl(semid, 0, GETALL, values);
+	printf("values: ");
+	for (i = 0; i < 4; i++) {
+		printf("%3i", values[i]);
+	}
+	printf("\n");
+	return 0;
+}
