8b5edf5ca092911edc235a14965562c9d82471e9
Pedro Perez <pperezm@tec.mx>
2019-10-22 08:12:20 -0500

    semaphores added

---
+++ ipc/semaphores/alternating/example1.c
@@ -0,0 +1,76 @@
+#include "header.h"
+
+#define PARENT	0
+#define CHILD 	1
+#define N 		5
+
+void child_process() {
+	int i, semid;
+	key_t key;
+
+	if ( (key = ftok("/dev/null", 127)) < 0 ){
+		perror("ftok");
+		exit(1);
+	}
+
+	if ( (semid = semget(key, 2, 0666)) < 0 ) {
+		perror("semget");
+		exit(-1);
+	}
+
+	for (i = 0; i < N; i++) {
+		sem_wait(semid, CHILD, 1);
+		printf("PARENT %i CHILD %i\n", getppid(), getpid());
+		sem_signal(semid, PARENT, 1);
+	}
+	exit(0);
+}
+
+void parent_process() {
+	int i, semid;
+	key_t key;
+
+	if ( (key = ftok("/dev/null", 127)) < 0 ){
+		perror("ftok");
+		exit(-1);
+	}
+	if ( (semid = semget(key, 2, 0666)) < 0 ) {
+		perror("semget");
+		exit(-1);
+	}
+	for (i = 0; i < N; i++) {
+		sem_wait(semid, PARENT, 1);
+		printf("PARENT %i CHILD %i\n", getppid(), getpid());
+		sleep(1);
+		sem_signal(semid, CHILD, 1);
+	}
+	exit(0);
+}
+
+int main(int argc, char* argv[]) {
+	int pid, i, semid;
+	key_t key;
+
+	if ( (key = ftok("/dev/null", 127)) < 0 ){
+		perror("ftok");
+		return -1;
+	}
+
+	if ( (semid = semget(key, 2, 0666 | IPC_CREAT)) < 0 ) {
+		perror("semget");
+		return -1;
+	}
+
+	semctl(semid, PARENT, SETVAL, 1);
+	semctl(semid, CHILD, SETVAL, 0);
+
+	if ( (pid = fork()) < 0 ) {
+		perror("fork");
+	} else if (pid == 0) {
+		child_process();
+	} else {
+		parent_process();
+		wait(NULL);
+	}
+	return 0;
+}
---
+++ ipc/semaphores/alternating/header.h
@@ -0,0 +1,30 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+
+int sem_wait(int semid, int sem_num, unsigned int val) {
+	struct sembuf op;
+
+	op.sem_num = sem_num;
+	op.sem_op = -val;
+	op.sem_flg = 0;
+	return semop(semid, &op, 1);
+}
+
+int sem_signal(int semid, int sem_num, unsigned int val) {
+	struct sembuf op;
+
+	op.sem_num = sem_num;
+	op.sem_op = val;
+	op.sem_flg = 0;
+	return semop(semid, &op, 1);
+}
+
+#endif
