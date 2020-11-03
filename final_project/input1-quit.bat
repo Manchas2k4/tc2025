2d5c1b82a841a68712503dee611ab0d200a4e294
Pedro Perez <pperezm@tec.mx>
2020-11-03 10:53:58 -0600

    test files for final projects added

---
+++ final_project/input1-quit.bat
@@ -0,0 +1,6 @@
+ls
+/bin/ls
+ls -l
+ls -l ; cat file1
+ls -l ; cat file1 ; grep de file2
+quit
---
+++ final_project/input2-noquit.bat
@@ -0,0 +1,6 @@
+; cat file1 ; grep foo file2
+cat file1 ; ; grep foo file2
+cat file1 ; ls -l ;
+cat file1 ; ; ; ; ls -l
+; ; ls -l
+;
